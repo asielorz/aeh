8c073c52678afd7e9d22468e461b9235a189288f
Asier Elorz <asielorz@gmail.com>
2021-02-21 16:01:43 +0100

    range_function is now in namespace aeh

--- aeh/src/range_function.hh
+++ aeh/src/range_function.hh
@@ -1,21 +1,26 @@
 #pragma once
 
-#include <functional>
 #include "debug/assert.hh"
+#include <functional>
 
-template <typename T>
-struct range_function
+namespace aeh
 {
-	template <std::convertible_to<std::function<T(int)>> F>
-	constexpr range_function(F const & subscript_, int size__) noexcept
-		: subscript(subscript_)
-		, size_(size__)
-	{}
 
-	[[nodiscard]] constexpr auto size() const noexcept -> int { return size_; }
-	[[nodiscard]] constexpr auto operator[] (int i) const noexcept -> T { debug_assert(i < size()); return subscript(i); }
+	template <typename T>
+	struct range_function
+	{
+		template <std::convertible_to<std::function<T(int)>> F>
+		constexpr range_function(F const & subscript_, int size__) noexcept
+			: subscript(subscript_)
+			, size_(size__)
+		{}
 
-private:
-	std::function<T(int)> subscript;
-	int size_;
-};
+		[[nodiscard]] constexpr auto size() const noexcept -> int { return size_; }
+		[[nodiscard]] constexpr auto operator[] (int i) const noexcept -> T { debug_assert(i >= 0 && i < size()); return subscript(i); }
+
+	private:
+		std::function<T(int)> subscript;
+		int size_;
+	};
+
+} // namespace aeh
