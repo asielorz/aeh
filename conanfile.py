4c874d6307140833b57a1e1518c7b67652f6789e
andoalon <andoalon97@gmail.com>
2022-09-15 00:48:30 +0200

    Update SDL. Change to SDL from conancenter. Get rid of bincrafters

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,6 @@ class AehConan(conans.ConanFile):
     description = "C++ utilities library"
     topics = ("utilities")
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package", "cmake_find_package_multi"
     options = {
         #"shared": [True, False], # Building as a DLL doesn't currently work
         "with_sdl2" : [True, False],
@@ -51,7 +50,7 @@ class AehConan(conans.ConanFile):
             self.requires("imgui/1.79")
 
         if self.options.with_sdl2:
-            self.requires("sdl2/2.0.14@bincrafters/stable")
+            self.requires("sdl/2.24.0")
             self.requires("opengl/system")
 
         self.requires("portable-file-dialogs/0.1.0")
